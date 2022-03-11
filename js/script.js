0cf16d5a67622db4dd12edc84faa9221322cd95a
GiovanniCalia <johnca@outlook.it>
2022-03-11 14:15:39 +0100

    Creazione repo e di js

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        arrSlides: [
+            {
+                title: "Svezia",
+                img: "01.jpg",
+                text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
+            },
+            {
+                title: "Norvegia",
+                img: "02.jpg",
+                text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
+            },
+            {
+                title: "Finlanda",
+                img: "03.jpg",
+                text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
+            },
+            {
+                title: "Irlanda",
+                img: "04.jpg",
+                text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
+            },
+            {
+                title: "Germania",
+                img: "05.jpg",
+                text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
+            },
+        ],
+    },
+});
